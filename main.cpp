47ef56cadda7b3b42f5bed1796c51910d1da4902
Krishna Jawale <krishnajawale7@gmail.com>
2026-08-30 10:40:52 -0400

    triangle rasterizing with barycentric coordinates

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "tgaimage.h"
 #include "model.h"
 
-constexpr int width  = 128;
-constexpr int height = 128;
+constexpr int width  = 1024;
+constexpr int height = 1024;
 
 constexpr TGAColor white  = {255, 255, 255, 255}; // attention, BGRA order
 constexpr TGAColor green  = {  0, 255,   0, 255};
@@ -54,56 +54,38 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 // viewpoint transformation
 std::tuple<int, int> project(Vec3f v) {
     return {
-        (v.x + 1.0f) * width / 2.0f,
-        (v.y + 1.0f) * height / 2.0f,
+        (v.x + 1.0f) * width * 0.5f,
+        (v.y + 1.0f) * height * 0.5f,
     };
+}
+
+// calculate signed area of triangle
+double signed_area(int x0, int y0, int x1, int y1, int x2, int y2) {
+    return (x0 * (y1 - y2) + x1 * (y2 - y0) + x2 * (y0 - y1)) * 0.5;
 }
 
 // draw a triangle
 void triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &framebuffer, TGAColor color) {
-    // sort vertices by y coordinate (y2 > y1 > y0)
-    if (y2 < y1) {
-        std::swap(y1, y2);
-        std::swap(x1, x2);
-    }
-    if (y2 < y0) {
-        std::swap(y0, y2);
-        std::swap(x0, x2);
-    } 
-    if (y1 < y0) {
-        std::swap(y0, y1);
-        std::swap(x0, x1);
-    }
+    // find bounding box of triangle
+    int xmin = std::min({x0, x1, x2});
+    int ymin = std::min({y0, y1, y2});
+    int xmax = std::max({x0, x1, x2});
+    int ymax = std::max({y0, y1, y2});
 
-    // if possible, fill bottom half of the triangle
-    if (y0 != y1) {
-        for (int y = y0; y <= y1; y++) {
-            int ax = x0 + (y - y0) * (x1 - x0) / (y1 - y0);
-            int bx = x0 + (y - y0) * (x2 - x0) / (y2 - y0);
+    // calculate signed area of triangle
+    double area = signed_area(x0, y0, x1, y1, x2, y2);
+    if (area == 0) return;
 
-            if (ax > bx) {
-                std::swap(ax, bx);
-            }
-            
-            // line(ax, y, bx, y, framebuffer, color);
-            for (int x = ax; x <= bx; x++) {
-                framebuffer.set(x, y, color);
-            }
-        }
-    }
+    // iterate over bounding box
+    for (int y = ymin; y <= ymax; y++) {
+        for (int x = xmin; x <= xmax; x++) {
+            // calculate weights (barycentric coordinates)
+            double w0 = signed_area(x, y, x1, y1, x2, y2) / area;
+            double w1 = signed_area(x0, y0, x, y, x2, y2) / area;
+            double w2 = signed_area(x0, y0, x1, y1, x, y) / area;
 
-    // if possible, fill top half of the triangle
-    if (y1 != y2) {
-        for (int y = y1; y <= y2; y++) {
-            int ax = x1 + (y - y1) * (x2 - x1) / (y2 - y1);
-            int bx = x0 + (y - y0) * (x2 - x0) / (y2 - y0);
-
-            if (ax > bx) {
-                std::swap(ax, bx);
-            }
-            
-            // line(ax, y, bx, y, framebuffer, color);
-            for (int x = ax; x <= bx; x++) {
+            // if all weights are positive, point is inside triangle
+            if (w0 >= 0 && w1 >= 0 && w2 >= 0) {
                 framebuffer.set(x, y, color);
             }
         }
@@ -111,14 +93,11 @@ void triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &framebuf
 }
 
 int main(int argc, char** argv) {
-    /*
     // load model
     Model model("models/diablo3.obj");
-    */
 
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    /*
     // draw faces
     for (const auto& face : model.faces) {
         // for each face, get the 3 corresponding vertices and project them to the screen
@@ -126,22 +105,13 @@ int main(int argc, char** argv) {
         auto [x1, y1] = project(model.verts[face.y]);
         auto [x2, y2] = project(model.verts[face.z]);
 
-        // draw the 3 lines of the face
-        line(x0, y0, x1, y1, framebuffer, red);
-        line(x1, y1, x2, y2, framebuffer, red);
-        line(x2, y2, x0, y0, framebuffer, red);
-    }
+        TGAColor rand_color;
+        for (int c = 0; c < 3; c++) {
+            rand_color[c] = std::rand() % 255;
+        }
 
-    // highlight vertices
-    for (const auto& vert : model.verts) {
-        auto [x, y] = project(vert);
-        framebuffer.set(x, y, green);
+        triangle(x0, y0, x1, y1, x2, y2, framebuffer, rand_color);
     }
-    */
-
-    triangle(  7, 45, 35, 100, 45,  60, framebuffer, red);
-    triangle(120, 35, 90,   5, 45, 110, framebuffer, white);
-    triangle(115, 83, 80,  90, 85, 120, framebuffer, green);
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
